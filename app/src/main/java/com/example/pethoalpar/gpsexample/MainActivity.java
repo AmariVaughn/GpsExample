e322c7cb9705267549c2b4b0dc5b9b190e195af2
Petho Alpar <Petho Alpar>
2016-04-13 20:49:48 +0300

    Gps Example

---
+++ app/src/main/java/com/example/pethoalpar/gpsexample/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.pethoalpar.gpsexample;
+
+import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private GpsTool gpsTool;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) this.findViewById(R.id.textView);
+        if(gpsTool == null){
+            gpsTool = new GpsTool(this);
+        }
+        Location location = gpsTool.getLocation();
+        Double longitude = location.getLongitude();
+        Double latittude = location.getLatitude();
+        Double altitude = location.getAltitude();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Longitude:").append(longitude).append("\n");
+        sb.append("Latitude:").append(latittude).append("\n");
+        sb.append("Altitide:").append(altitude);
+        textView.setText(sb.toString());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gpsTool.stopGpsUpdate();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gpsTool.startGpsUpdate();
+    }
+}
