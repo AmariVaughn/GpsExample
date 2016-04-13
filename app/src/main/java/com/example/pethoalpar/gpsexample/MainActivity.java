cf15ee9bc8e5374efd03c5a6ae1778a3e9fdc1ba
pethoalpar <pethoalpar@gmail.com>
2016-04-13 22:27:00 +0300

    Correction

--- app/src/main/java/com/example/pethoalpar/gpsexample/MainActivity.java
+++ app/src/main/java/com/example/pethoalpar/gpsexample/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.pethoalpar.gpsexample;
 
 import android.location.Location;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         StringBuilder sb = new StringBuilder();
         sb.append("Longitude:").append(longitude).append("\n");
         sb.append("Latitude:").append(latittude).append("\n");
-        sb.append("Altitide:").append(altitude);
+        sb.append("Altitude:").append(altitude);
         textView.setText(sb.toString());
     }
 
